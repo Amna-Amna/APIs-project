b1b8684a9ba77c31b4a730ccced0b51325ad711e
Amna-Amna <mailtoamna4@gmail.com>
2023-08-25 23:16:07 +0500

    feat: Implementing API Data Retrieval and Display Functionality on the Web

--- script.js
+++ script.js
@@ -2,3 +2,42 @@
 
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
+
+const getCountryData = function(country){
+const request = new XMLHttpRequest();
+request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+request.send();
+
+request.addEventListener('load', function () {
+  const [data] = JSON.parse(this.responseText);
+  console.log(data);
+
+  const html = `
+    <article class="country">
+      <img class="country__img" src="${data.flags.svg}" />
+      <div class="country__data">
+        <h3 class="country__name">${data.name.common}</h3>
+        <h4 class="country__region">${data.region}</h4>
+        <p class="country__row"><span>👫</span>${(
+          +data.population / 1000000
+        ).toFixed(1)}</p>
+        <p class="country__row"><span>🗣️</span>${Object.values(
+          data.languages
+        ).join(', ')}</p>
+        <p class="country__row"><span>💰</span>${Object.keys(
+          data.currencies
+        ).join(', ')}</p>
+      </div>
+    </article>
+  `;
+
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+})
+};
+
+getCountryData('india')
+getCountryData('pakistan')
+getCountryData('usa')
+getCountryData('germany')
+
